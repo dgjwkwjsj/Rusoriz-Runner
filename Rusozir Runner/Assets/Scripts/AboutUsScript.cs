a597d6f092af7b035c7856693ec5f71e6f06247b
tr0yanda <138056162+dgjwkwjsj@users.noreply.github.com>
2026-08-08 14:30:31 +0300

    commit 26

---
+++ Rusozir Runner/Assets/Scripts/AboutUsScript.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class AboutUsScript : MonoBehaviour
+{
+    public void AboutUs()
+    {
+        SceneManager.LoadScene(2);
+    }
+}
