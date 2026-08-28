77183c84dc92f4b7eb9fb12cbf4296ce65f5643d
tr0yanda <138056162+dgjwkwjsj@users.noreply.github.com>
2026-08-28 17:29:27 +0300

    commit 32

---
+++ Rusozir Runner/Assets/Scripts/ShopScene.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ShopScene : MonoBehaviour
+{
+   
+    public void StartShop()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
